12978a773ff6794cd17235160883ef7cfaee2425
yvain13-coder <yvain_13@hotmail.com>
2026-04-23 09:31:39 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AutoIA from './AutoIA.jsx'
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AutoIA />
+  </React.StrictMode>,
+)
